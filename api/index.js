68bff4922c7129bd2f20de06839b8bd462602c0c
DSamuelEC <samueledcardenas@gmail.com>
2023-09-26 15:40:50 -0500

    arr2

--- api/index.js
+++ api/index.js
@@ -6,4 +6,4 @@ conn.sync({ force: true }).then(() => {
   server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
-});
+}); //comentario random
