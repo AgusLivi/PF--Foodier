81b2b3594685e85b9851f131bb933cbdddaf139a
Ayrton H. Torres <ayrton.torres.1995@gmail.com>
2023-09-26 17:05:00 -0300

    arreglando mi cagada

--- api/src/controllers/controladorPrueva.js
+++ api/src/controllers/controladorPrueva.js
@@ -30,7 +30,7 @@ const cargarPruevas = async () =>{
     const produto1a = await Product.create(PanPepes, )
 
     const produto1b = await Product.create(pebetesPepes)
-    await produto1b.addSeller(seller1.Seller_ID)
+
 
 
 
