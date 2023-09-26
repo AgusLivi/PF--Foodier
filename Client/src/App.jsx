c397f5ae603b675fd878d5302caf488b02a370aa
AgustinLiviñi <agustinlivini07@gmail.com>
2023-09-26 20:21:53 -0300

    homePage

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,6 +5,7 @@ import PerfilUsuario from './Views/PerfilUsuario/PerfilUsuario'
 import NavBar from './Components/NavBar/NavBar'
 import Login from './Components/Login/Login';
 import Landing from './Views/Landing/landing'
+import HomePage from './Views/Home/Home';
 
 
 const App = () => {
@@ -17,6 +18,7 @@ const App = () => {
     <Route path='/' element={<Landing/>}/>
     <Route path='/profile' element={<PerfilUsuario/>}/>
     <Route path='/login' element={<Login/>}/>
+    <Route path='/home' element={HomePage}/>
     </Routes>
 
     </div>
--- Client/src/Components/NavBar/NavBar.jsx
+++ Client/src/Components/NavBar/NavBar.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logoFoodier.png'; // Asegúrate de que la ruta sea correcta
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div>
+      
+        <div><Link to="/Ubicacion"></Link></div>
+        <div><Link to="/Shops"></Link></div>
+        <div>
+          <img src={logo} alt='logo'/>
+        </div>
+      
+    </div>
   )
 }
 
