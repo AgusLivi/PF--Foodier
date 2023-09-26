7d2f863793c5606b63739fb12f699f4cbbbbe1e6
DSamuelEC <samueledcardenas@gmail.com>
2023-09-26 11:15:31 -0500

    feat(/client): add apiPrueba Folder, api.js

---
+++ Client/apiPrueba/api.js
@@ -0,0 +1,74 @@
+const api = [
+    {
+        nombreProducto: 'Hamburguesa sencilla',
+        cantidad: 10,
+        precio: '$500',
+        imagenProducto: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Hamburger_%28black_bg%29.jpg/800px-Hamburger_%28black_bg%29.jpg',
+        descripcionProducto: 'Hamburguesa sencilla con pepinillos',
+        categoria: ['Hamburguesa'],
+        fechaFabricacion: '12-23-2023'
+    },
+    {
+        nombreProducto: 'Pizza 4 quesos',
+        cantidad: 8,
+        precio: '$300',
+        imagenProducto: 'https://www.elespectador.com/resizer/zGywOSIEHqfbrj_yVF-Upui3iMk=/arc-anglerfish-arc2-prod-elespectador/public/D2KL4RRCRJA7RJC5TNULFZEKR4.jpg',
+        descripcionProducto: 'Pizza con 4 quesos',
+        categoria: ['Pizza'],
+        fechaFabricacion: '12-23-2023'
+    },
+    {
+        nombreProducto: 'Pizza deluxe',
+        cantidad: 15,
+        precio: '$600',
+        imagenProducto: 'https://cdn.colombia.com/gastronomia/2011/08/25/pizza-margarita-3684.webp',
+        descripcionProducto: 'Pizza deluxe con muchos ingredientes',
+        categoria: ['Pizza'],
+        fechaFabricacion: '12-23-2023'
+    },
+    {
+        nombreProducto: 'Hamburguesa deluxe',
+        cantidad: 20,
+        precio: '$800',
+        imagenProducto: 'https://cloudfront-us-east-1.images.arcpublishing.com/elespectador/7DSXKTGTTJBUFJVUMKPMFEGFOU.jpg',
+        descripcionProducto: 'Hamburguesa deluxe con muchos ingredientes',
+        categoria: ['Hamburguesa'],
+        fechaFabricacion: '12-23-2023'
+    },
+    {
+        nombreProducto: 'Sushi Promo Pack',
+        cantidad: 10,
+        precio: '$800',
+        imagenProducto: 'https://imag.bonviveur.com/sushi-casero.jpg',
+        descripcionProducto: '30 bocados de sushi',
+        categoria: ['Sushi'],
+        fechaFabricacion: '12-23-2023'
+    },
+    {
+        nombreProducto: 'Sushi Sencillo',
+        cantidad: 6,
+        precio: '$300',
+        imagenProducto: 'https://assets.unileversolutions.com/recipes-v2/237341.jpg',
+        descripcionProducto: 'Sushi con tempura y cangrejo',
+        categoria: ['Sushi'],
+        fechaFabricacion: '12-23-2023'
+    },
+    {
+        nombreProducto: 'Pasta sencilla con parmesano',
+        cantidad: 12,
+        precio: '$500',
+        imagenProducto: 'https://www.lavanguardia.com/files/image_948_465/files/fp/uploads/2020/09/09/5f58b1bb6d322.r_d.627-418.jpeg',
+        descripcionProducto: 'Pasta con parmesano y oliva',
+        categoria: ['Pasta'],
+        fechaFabricacion: '12-23-2023'
+    },
+    {
+        nombreProducto: 'Pasta carbonara',
+        cantidad: 16,
+        precio: '$700',
+        imagenProducto: 'https://i.ytimg.com/vi/VKizSq2_ZNs/maxresdefault.jpg',
+        descripcionProducto: 'Pasta carbonara con parmesano',
+        categoria: ['Pasta'],
+        fechaFabricacion: '12-23-2023'
+    },
+]
