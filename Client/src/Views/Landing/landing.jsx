4b7914481f379053f870c027cc22fba1a1bc870f
FerTassara <nanotassara@live.com>
2023-09-26 18:23:06 -0300

    correcion module.css

--- Client/src/Views/Landing/landing.jsx
+++ Client/src/Views/Landing/landing.jsx
@@ -6,7 +6,7 @@ const landing = () => {
         <div>
                 <h1>COMBATAMOS JUNTOS EL desperdicio de alimentos</h1>
                 <p>¿Tienes hambre? Con la aplicación FOODIER puedes salvar comida deliciosa de restaurantes y establecimientos de tu zona por muy poco dinero.</p>
-                <Link to={'/Autentication'}><button>Identificate</button></Link>
+                <Link to={'/Autentication'}><button>Identificate!</button></Link>
         </div> 
     )
 }
