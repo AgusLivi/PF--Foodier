c7efdc67bba4599f75c2bb8a5bc6f3ff1afea58f
AgustinLiviñi <agustinlivini07@gmail.com>
2023-09-27 12:38:43 -0300

    login

--- Client/src/Components/Login/Login.jsx
+++ Client/src/Components/Login/Login.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import GoogleLogin from 'react-google-login'
+import { useEffect } from 'react'
 //import style from './Login.module.css'
 
 const Login = () => {
-  return (
+  const [user, setUser] = useState({});
+  const  clientID = '213587509979-9539rfrnm5e9bf8m8r1mj5tl15rhjej0.apps.googleusercontent.com'
+  
+  useEffect(()=>{
+    const start = () => {
+      gapi.auth2.init({
+        clientId: clientID,
+      })
+    }
+    gapi.load("client:auth2", start)
+  }, [])
+
+  const onSucces = (response) => {
+    setUser(response.profileObj)
+  }
+    return (
     <div>
       <div>
         <h1>Lorem ipsum dolor sit amet consectetur adipisicing</h1>
 
       </div>
-    
-       <button>Continuar con Google</button>
-       <button>Crear Cuenta</button>
-
+      <div className={user? "profile":"hidden"}>
+        <img src={user.imageUrl} alt="" />
+        <h3>{user.name}</h3>
+      </div>
 
       <div>
         <p>Al registrarte aceptas los
