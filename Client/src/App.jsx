d49a79dbdb7416001c6a669c8a1935d5af7c3064
alisonroa <roa.alisonn@gmail.com>
2023-09-28 10:49:08 -0300

    update

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import PerfilUsuario from './Views/PerfilUsuario/PerfilUsuario'
 import NavBar from './Components/NavBar/NavBar'
 import Login from './Components/Login/Login';
-import Landing from './Views/Landing/landing'
+import Landing from './Views/Landing/Landing'
 import FormUser from "./Components/FormUser/FormUser";
 import PerfilNegocio from "./Views/PerfilNegocio/PerfilNegocio";
 import Footer from "./Components/Footer/Footer";
@@ -15,9 +15,9 @@ const App = () => {
       {location.pathname !== "/" && <NavBar />}
       <Routes>
         <Route path='/' element={<Landing />} />
-        <Route path='/profile' element={<PerfilUsuario />} />
+        <Route path='/profile' element={<PerfilUsuario />} /> //esta vacio
         <Route path='/login' element={<Login />} />
-        <Route path='/business' element={<PerfilNegocio />} />
+        <Route path='/business' element={<PerfilNegocio />} /> //vacio
         <Route path='/formuser' element={<FormUser />} />
       </Routes>
       {location.pathname !== "/" && <Footer />}
