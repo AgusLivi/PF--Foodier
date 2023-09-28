15d3ecc5929cfe2265ff47f8bf146b13028e2ff9
IVANH <ivan.hd.16@gmail.com>
2023-09-28 12:23:20 -0300

    rutas y handlers

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 //Importo y mas abajo agrego la ruta para el payment
 const paymentController = require('../controllers/paymentController');
+const favController = require('../controllers/favController');
 
 // import de los controler
 const { getAllPost, getPostById, createPost} = require('../controllers/postController')
@@ -11,6 +12,14 @@ const { getAllPost, getPostById, createPost} = require('../controllers/postContr
 router.post('/post', createPost)
 
 router.post('/create-payment', paymentController.createPayment);
+
+// Ruta para agregar un vendedor a favoritos
+router.post('/add-favorites', favController.addFavorites);
+
+// Ruta para obtener la lista de vendedores favoritos de un usuario
+router.get('/get-favorites/:user_ID', favController.getFavorites);
+
+router.use('/products', productRoutes)
 
 
 module.exports = router;
