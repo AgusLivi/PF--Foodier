9a3a8a775e699566186c768160ecdc0a9d236dca
FerTassara <nanotassara@live.com>
2023-09-28 13:29:33 -0300

    cardcontainer ok

---
+++ Client/src/Components/CardCointainer/cardcontainer.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Card from './Card'; 
+
+const CardContainer = ({ cards, onAddToCart }) => {
+    return (
+        <div className='cardContainer'>
+            {cards.map((card) => (
+                <Card
+                    key={card.Product_ID}
+                    Product_ID={card.Product_ID}
+                    Fecha={card.Fecha}
+                    Nombre={card.Nombre}
+                    Descripcion={card.Descripcion}
+                    Precio={card.Precio}
+                    Imagen={card.Imagen}
+                    Cantidad={card.Cantidad}
+                    Categoria={card.Categoria}
+                    onAddToCart={onAddToCart}
+                />
+            ))}
+        </div>
+    );
+}
+
+export default CardContainer;
