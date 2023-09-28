d726ba398861bd37abc06a6a2f3d50ecc63fba5c
AgustinLiviñi <agustinlivini07@gmail.com>
2023-09-28 11:47:27 -0300

    reducer NO MERGEAR

--- Client/src/Redux/reducer.js
+++ Client/src/Redux/reducer.js
@@ -1,20 +1,56 @@
 import {
-    CURRENT_PAGE
+    GET_ALL_PRODUCTS,
+    SEARCH_BY_NAME,
+    SEARCH_SELLER_BY_ID,
+    CREATE_PRODUCT,
+    GET_ALL_FAV,
 } from './actionsType'
 
 const initialState = { 
-    currentPage: 1,
+    products: [],
+    productsCopy: [],
+    createdProduct: [],
+    sellersFav:[],
+    productsByName: [],
 }
 
 const reducer = (state = initialState, {type, payload}) => {
+   let filteredProducts = []
 
     switch(type){
-        case CURRENT_PAGE:
-            case CURRENT_PAGE:
+        case GET_ALL_PRODUCTS:
+            case GET_ALL_PRODUCTS:
                 return {
                     ...state,
-                    currentPage: payload,
+                    products: payload,
+                    productsCopy: payload,
+                    productsByName: payload
                 }
+
+            case SEARCH_BY_NAME:
+                return {
+                    ...state,
+                    products: filteredProducts,
+                    productsByName: filteredProducts,
+                }
+
+            case CREATE_PRODUCT: 
+                return {
+                    ...state,
+                    createdProduct: [...state.createdProduct, payload]
+                }
+            case SEARCH_SELLER_BY_ID:
+                return {
+                    ...state,
+                    products: payload,
+                }
+            case GET_ALL_FAV: 
+                return {
+                    ...state,
+                    sellersFav: payload,
+                }
+            default:
+                return state;
     }
 
 
