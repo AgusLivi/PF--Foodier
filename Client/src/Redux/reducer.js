d43be4b740e2eb809f0fd015d052abe746c614cf
AgustinLiviñi <agustinlivini07@gmail.com>
2023-09-29 19:09:41 -0300

    reducer todavia le falta

--- Client/src/Redux/reducer.js
+++ Client/src/Redux/reducer.js
@@ -1,73 +1,83 @@
-import {
-    GET_ALL_PRODUCTS,
-    SEARCH_BY_NAME,
-    SEARCH_BY_ID,
-    CREATE_PRODUCT,
-    GET_ALL_FAV,
-    GET_FILTER,
-    DELETE_PRODUCT
-} from './actionsType'
+    import {
+        GET_ALL_PRODUCTS,
+        SEARCH_BY_NAME,
+        SEARCH_BY_ID,
+        CREATE_PRODUCT,
+        GET_ALL_FAV,
+        DELETE_PRODUCT,
+        POST_FAVORITES,
+        CREATE_USER,
+        GET_SELLER_FAV,
+        CATEGORIES,
+        ADDRESS,
+        AVERAGE_RATING,
+        PAYMENT,
+        ORDER_BY,
+        ORDER,
+        PAGE,
+        PAGE_SIZE,
+        CREATE_POST,
+        CREATE_PAYMENT,
+    } from './actionsType';
 
-const initialState = { 
-    products: [],
-    productsCopy: [],
-    createdProduct: [],
-    sellersFav:[],
-    productsByName: [],
-    filtereds: []
-}
+    const initialState = { 
+        products: [],
+        productsCopy: [],
+        createdProduct: [],
+        sellersFav: [],
+        productsByName: [],
+        filtereds: [],
+        //tengo q agregar mas
+    };
 
-const reducer = (state = initialState, {type, payload}) => {
-   let filteredProducts = []
+    const reducer = (state = initialState, { type, payload }) => {
+        let filteredProducts = [];
 
-    switch(type){
+        switch (type) {
             case GET_ALL_PRODUCTS:
                 return {
                     ...state,
                     products: payload,
                     productsCopy: payload,
-                    productsByName: payload
-                }
+                    productsByName: payload,
+                };
 
             case SEARCH_BY_NAME:
                 return {
                     ...state,
-                    products: filteredProducts,
-                    productsByName: filteredProducts,
-                }
+                    productsByName: payload, // Actualizar solo la propiedad productsByName
+                };
 
-            case CREATE_PRODUCT: 
+            case CREATE_PRODUCT:
                 return {
                     ...state,
-                    createdProduct: [...state.createdProduct, payload]
-                }
+                    createdProduct: [...state.createdProduct, payload],
+                };
+
             case SEARCH_BY_ID:
                 return {
                     ...state,
                     products: payload,
-                }
-            case GET_ALL_FAV: 
+                };
+
+            case GET_ALL_FAV:
                 return {
                     ...state,
                     sellersFav: payload,
-                }
-            case GET_FILTER:
-                return {
-                    ...state,
-                    filtereds: payload
-                }
-            case DELETE_PRODUCT: 
+                };
+
+            case DELETE_PRODUCT:
                 return {
                     ...state,
                     products: payload,
-                    productsCopy: payload
-                }
+                    productsCopy: payload,
+                };
+
+                
+
             default:
                 return state;
-    }
+        }
+    };
 
-
-
-
-
-}
+    export default reducer;
