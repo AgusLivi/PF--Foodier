b2b655f37dbc110b7ebbb76bd552ba6e3c06b608
AgustinLiviñi <agustinlivini07@gmail.com>
2023-09-29 11:36:55 -0300

    arreglo en el reducer

--- Client/src/Redux/reducer.js
+++ Client/src/Redux/reducer.js
@@ -34,6 +34,7 @@ const reducer = (state = initialState, {type, payload}) => {
                 return {
                     ...state,
                     products: filteredProducts,
+                    productsCopy
                     productsByName: filteredProducts,
                 }
 
