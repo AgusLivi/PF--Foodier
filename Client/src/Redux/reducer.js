499dbd1d8b1a077e95dd2b505e019b374b62ca2a
AgustinLiviñi <agustinlivini07@gmail.com>
2023-09-29 19:13:03 -0300

    reducer todavvia sin finalizar

--- Client/src/Redux/reducer.js
+++ Client/src/Redux/reducer.js
@@ -1,75 +1,117 @@
-import {
-    GET_ALL_PRODUCTS,
-    SEARCH_BY_NAME,
-    SEARCH_BY_ID,
-    CREATE_PRODUCT,
-    GET_ALL_FAV,
-    GET_FILTER,
-    DELETE_PRODUCT
-} from './actionsType'
+    import {
+        GET_ALL_PRODUCTS,
+        SEARCH_BY_NAME,
+        SEARCH_BY_ID,
+        CREATE_PRODUCT,
+        GET_ALL_FAV,
+        DELETE_PRODUCT,
+        POST_FAVORITES,
+        CREATE_USER,
+        GET_SELLER_FAV,
+        CATEGORIES,
+        ADDRESS,
+        AVERAGE_RATING,
+        PAYMENT,
+        ORDER_BY,
+        ORDER,
+        PAGE,
+        PAGE_SIZE,
+        CREATE_POST,
+        CREATE_PAYMENT,
+    } from './actionsType';
 
-const initialState = { 
-    products: [],
-    productsCopy: [],
-    createdProduct: [],
-    sellersFav:[],
-    productsByName: [],
-    filtereds: []
-}
+    const initialState = { 
+        products: [],
+        productsCopy: [],
+        createdProduct: [],
+        sellersFav: [],
+        productsByName: [],
+        postedFiltereds: [],
+        createdUser: [],
+        selectedCategories: [],
+        selectedOrderBy: [],
+        selectedOrder: [],
+        createdPost: [],
+        createPayment: [],
+        averageRating: [],
+        address: [],
+        //tengo q agregar mas
+    };
 
-const reducer = (state = initialState, {type, payload}) => {
-   let filteredProducts = []
+    const reducer = (state = initialState, { type, payload }) => {
 
-    switch(type){
-        case GET_ALL_PRODUCTS:
+        switch (type) {
             case GET_ALL_PRODUCTS:
                 return {
                     ...state,
                     products: payload,
                     productsCopy: payload,
-                    productsByName: payload
-                }
+                    productsByName: payload,
+                };
 
             case SEARCH_BY_NAME:
                 return {
                     ...state,
-                    products: filteredProducts,
-                    productsCopy
-                    productsByName: filteredProducts,
-                }
+                    productsByName: payload, // Actualizar solo la propiedad productsByName
+                };
 
-            case CREATE_PRODUCT: 
+            case CREATE_PRODUCT:
                 return {
                     ...state,
-                    createdProduct: [...state.createdProduct, payload]
-                }
-            case SEARCH_SELLER_BY_ID:
+                    createdProduct: [...state.createdProduct, payload],
+                };
+
+            case SEARCH_BY_ID:
                 return {
                     ...state,
                     products: payload,
-                }
-            case GET_ALL_FAV: 
+                };
+
+            case GET_ALL_FAV:
+                return {
+                    ...state,
+                    sellersFav: payload,
+                };
+
+            case DELETE_PRODUCT:
+                return {
+                    ...state,
+                    products: payload,
+                    productsCopy: payload,
+                };
+            case POST_FAVORITES:
                 return {
                     ...state,
                     sellersFav: payload,
                 }
-            case GET_FILTER:
+            case CREATE_USER: 
                 return {
                     ...state,
-                    filtereds: payload
+                    createdUser: payload,
                 }
-            case DELETE_PRODUCT: 
+
+            case GET_SELLER_FAV: 
                 return {
                     ...state,
-                    products: payload,
-                    productsCopy: payload
+                    sellersFav: payload
                 }
+            case CATEGORIES:
+                return {
+                    ...state,
+
+
+                }
+
+            case ADDRESS:
+                return {
+                    ...state
+                }
+
+                
+
             default:
                 return state;
-    }
+        }
+    };
 
-
-
-
-
-}
+    export default reducer;
