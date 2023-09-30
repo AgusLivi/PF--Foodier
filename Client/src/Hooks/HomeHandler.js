0961687f7dbe11c3991ddb60aca8fa1e2dbb8481
AgustinLiviñi <agustinlivini07@gmail.com>
2023-09-30 15:47:20 -0300

    Home handler

--- Client/src/Hooks/HomeHandler.js
+++ Client/src/Hooks/HomeHandler.js
@@ -1,7 +1,21 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { useState, useEffect } from 'react'
-
+import {
+    getAllProducts,
+    postFav,
+    categoriesFilter,
+    addressFilter,
+    average_rating,
+    orderBy,
+    orderUpDown,
+    selectedCategories
+} from '../Redux/actions'
 const homeHandler = () => {
+    //estados globales
+    const selectedCategories = useSelector(state => state.selectedCategories)
+    
+
+
  
     
     return {
