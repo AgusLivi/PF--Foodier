54e50330234da668a1f29e848dc1565db7823613
AgustinLiviñi <agustinlivini07@gmail.com>
2023-09-30 15:39:17 -0300

    home handler

--- Client/src/Hooks/HomeHandler.js
+++ Client/src/Hooks/HomeHandler.js
@@ -1,2 +1,12 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { useState, useEffect } from 'react'
+
+const homeHandler = () => {
+ 
+    
+    return {
+
+    }
+}
+
+export default homeHandler
