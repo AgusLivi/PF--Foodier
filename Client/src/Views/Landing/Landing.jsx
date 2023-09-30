19d135be214657b6de965baea884465df9be5ce7
FerTassara <nanotassara@live.com>
2023-09-30 19:50:44 -0300

    modal en landing

--- Client/src/Views/Landing/Landing.jsx
+++ Client/src/Views/Landing/Landing.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import Modal from 'react-modal';
 import BannerBackground from "../../assets/home-banner-background.png";
 import BannerImage from "../../assets/home-banner-image.png";
 import PickMeals from "../../assets/pick-meals-image.png";
@@ -12,8 +13,19 @@ import Logo from "../../assets/Logo.png";
 import style from './Landing.module.css'
 import { Link } from 'react-router-dom'
 
-
 const Home = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  useEffect(() => {
+    // Abre el modal automáticamente después de 2 segundos
+    const timer = setTimeout(() => {
+      setModalIsOpen(true);
+    }, 2000);
+
+    // Limpia el temporizador cuando el componente se desmonta
+    return () => clearTimeout(timer);
+  }, []);
+
   const workInfoData = [
     {
       image: PickMeals,
@@ -32,105 +44,112 @@ const Home = () => {
     },
   ];
 
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
   return (
     <>
-     {/*NavBar sectionn*/}
-     <nav>
-      <div className={style.navlogocontainer}>
-        <img src={Logo} alt="" />
-      </div>     
-     </nav>
+      <nav>
+        <div className={style.navlogocontainer}>
+          <img src={Logo} alt="" />
+        </div>     
+      </nav>
 
-    <div className={style.homecontainer}>
-      {/*Home Section*/}
-
-      <div className={style.homebannercontainer}>
-        <div className={style.homebannerimagecontainer}>
-          <img src={BannerBackground} alt="" />
-        </div>
-        <div className={style.hometextsection}>
-          <h1 className={style.primaryheading}>
-            Combatamos el desperdicio de comida
-          </h1>
-          <p className={style.primarytext}>
-          Con Foodier podrás salvar el excedente de tus negocios favoritos y darle un buen final.
-          </p>
-          <Link className={style.link} to={`/login`}>
-          <button className={style.secondarybutton}>
-            Entrar <FiArrowRight />{" "}
-          </button>
-          </Link>
-        </div>
-        <div className={style.homeimagesection}>
-          <img src={BannerImage} alt="" />
+      <div className={style.homecontainer}>
+        <div className={style.homebannercontainer}>
+          <div className={style.homebannerimagecontainer}>
+            <img src={BannerBackground} alt="" />
+          </div>
+          <div className={style.hometextsection}>
+            <h1 className={style.primaryheading}>
+              Combatamos el desperdicio de comida
+            </h1>
+            <p className={style.primarytext}>
+              Con Foodier podrás salvar el excedente de tus negocios favoritos y darle un buen final.
+            </p>
+            <Link className={style.link} to={`/login`}>
+              <button className={style.secondarybutton}>
+                Entrar <FiArrowRight />{" "}
+              </button>
+            </Link>
+          </div>
+          <div className={style.homeimagesection}>
+            <img src={BannerImage} alt="" />
+          </div>
         </div>
       </div>
-      </div>
-
-      {/*Work Section*/}
 
       <div className={style.worksectionwrapper}>
-      <div className={style.worksectiontop}>
-        <h1 className={style.primaryheading}>¿Cómo funciona?</h1>
-        <p className={style.primarytext}>
-        ¿Te ha sobrado producto al final de la jornada? En Foodier podemos ayudarte. Haz como miles de negocios en todo el mundo y vende tu excedente a los clientes.
-        </p>
-      </div>
-      <div className={style.worksectionbottom}>
-        {workInfoData.map((data) => (
-          <div className={style.worksectioninfo} key={data.title}>
-            <div className={style.infoboxesimgcontainer}>
-              <img src={data.image} alt="" />
+        <div className={style.worksectiontop}>
+          <h1 className={style.primaryheading}>¿Cómo funciona?</h1>
+          <p className={style.primarytext}>
+            ¿Te ha sobrado producto al final de la jornada? En Foodier podemos ayudarte. Haz como miles de negocios en todo el mundo y vende tu excedente a los clientes.
+          </p>
+        </div>
+        <div className={style.worksectionbottom}>
+          {workInfoData.map((data) => (
+            <div className={style.worksectioninfo} key={data.title}>
+              <div className={style.infoboxesimgcontainer}>
+                <img src={data.image} alt="" />
+              </div>
+              <h2>{data.title}</h2>
+              <p>{data.text}</p>
             </div>
-            <h2>{data.title}</h2>
-            <p>{data.text}</p>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
 
- {/*Contact Sections*/}
-    <div className={style.contactpagewrapper}>
-      <h1 className={style.primaryheading}>Tienes alguna pregunta?</h1>
-      <h1 className={style.primaryheading}>Contáctanos!</h1>
-      <div className={style.contactformcontainer}>
-        <input type="text" placeholder="tucorreo@gmail.com" />
-        <button className={style.secondarybutton}>Enviar</button>
+      <div className={style.contactpagewrapper}>
+        <h1 className={style.primaryheading}>Tienes alguna pregunta?</h1>
+        <h1 className={style.primaryheading}>Contáctanos!</h1>
+        <div className={style.contactformcontainer}>
+          <input type="text" placeholder="tucorreo@gmail.com" />
+          <button className={style.secondarybutton}>Enviar</button>
+        </div>
       </div>
-    </div>
 
-   {/*Footer Section*/}
       <div className={style.footerwrapper}>
-      <div className={style.footersectionone}>
-        <div className={style.footerlogocontainer}>
-          <img src={Logo} alt="" />
+        <div className={style.footersectionone}>
+          <div className={style.footerlogocontainer}>
+            <img src={Logo} alt="" />
+          </div>
+          <div className={style.footericons}>
+            <a>
+              <SiLinkedin />
+            </a>
+            <a href="https://www.instagram.com/foodierapp/">
+              <BiLogoInstagram/>
+            </a>
+            <a href="https://www.facebook.com/foodierx/">
+              <FaFacebookF />
+            </a>
+          </div>
         </div>
-        <div className={style.footericons}>
-          <a>
-           <SiLinkedin />
-          </a>
-          <a href="https://www.instagram.com/foodierapp/">
-          <BiLogoInstagram/>
-          </a>
-          <a href="https://www.facebook.com/foodierx/">
-          <FaFacebookF />
-          </a>
+
+        <div className={style.footersectiontwo}>
+          <div className={style.footersectioncolumns}>
+            <span>1136183553</span>
+            <span>helpfoodier@gmail.com</span>
+          </div>
+          <div className={style.footersectioncolumns}>
+            <span>Términos y Condiciones</span>
+            <span>Política de Privacidad</span>
+          </div>
         </div>
       </div>
 
-      <div className={style.footersectiontwo}>
-       
-        <div className={style.footersectioncolumns}>
-          <span>1136183553</span>
-          
-          <span>helpfoodier@gmail.com</span>
-        </div>
-        <div className={style.footersectioncolumns}>
-          <span>Términos y Condiciones</span>
-          <span>Política de Privacidad</span>
-        </div>
-      </div>
-    </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Ejemplo de Modal"
+      >
+        <h2>Sabias estos datos?</h2>
+      <p>El desperdicio de alimentos es responsable del 10% de las emisiones de gases de efecto invernadero.</p>
+      <p>Desperdiciamos 2500 millones de toneladas de comida mientras 828 millones de personas pasan hambre.</p>
+      <p>Este desperdicio nos cuesta 1,2 billones de dolares por año.</p>
+        <button onClick={closeModal}>Cerrar Modal</button>
+      </Modal>
     </>
   );
 };
