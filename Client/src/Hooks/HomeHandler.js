92582880dbd3401899799ded36faa88846e1f5d1
AgustinLiviñi <agustinlivini07@gmail.com>
2023-09-30 16:11:41 -0300

    hook eliminado

--- Client/src/Hooks/HomeHandler.js
+++ Client/src/Hooks/HomeHandler.js
@@ -12,29 +12,37 @@ import {
 } from '../Redux/actions'
 const homeHandler = () => {
     //global state 
-    const selectedCategories = useSelector(state => state.selectedCategories)
-    const orderBy = useSelector(state => state.orderBy)
-    const orderUpDown = use (state => state.selectedOrder)
+    // const selectedCategories = useSelector(state => state.selectedCategories)
+    // const orderBy = useSelector(state => state.orderBy)
+    // const orderUpDown = use (state => state.selectedOrder)
 
-    //local state
-    const [selectedOrder, setSelectedOrder] = useState([])
-    const [selectedCat, setSelectedCat] = useState([])
-    const [categoriesError, setCategoriesError] = useState(false)
-    const [orderError, setOrderError] = useState(false)
-    const [reset, setReset] = useState([])
-    const dispatch = useDispatch()
+    // //local state
+    // const [selectedOrder, setSelectedOrder] = useState([])
+    // const [selectedCat, setSelectedCat] = useState([])
+    // const [categoriesError, setCategoriesError] = useState(false)
+    // const [orderError, setOrderError] = useState(false)
+    // const [reset, setReset] = useState([])
+    // const dispatch = useDispatch()
 
-    useEffect(()=>{
-        setSelectedCat(selectedCategories)
-        setSelectedOrder(orderBy)
-    }, [orderError, categoriesError])
+    // useEffect(()=>{
+    //     setSelectedCat(selectedCategories)
+    //     setSelectedOrder(orderBy)
+    // }, [orderError, categoriesError])
 
-    useEffect (() => {
-        setReset(false)
-    },[reset])
- 
+    // useEffect (() => {
+    //     setReset(false)
+    // },[reset])
     
+    // //handlers
+    // const homeFrom = selected => {
+    //     try {
+          
+    //     }
+    // }
+
     return {
+
+
 
     }
 }
