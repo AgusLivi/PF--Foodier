7502d135f497a2543a0d3cff4cf3d5f9b85d82f9
alisonroa <roa.alisonn@gmail.com>
2023-10-01 18:01:32 -0300

    update

--- Client/src/Components/CardContainer/CardContainer.jsx
+++ Client/src/Components/CardContainer/CardContainer.jsx
@@ -1,30 +1,31 @@
 import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { getAllProducts } from '../../Redux/actions';
-import Card from '../Card/card'; 
+import Card from '../Card/card';
+import styles from './CardContainer.module.css';
 
-const Cards = () => {
-  const dispatch = useDispatch();
-  const products = useSelector(state => state.products); 
-  useEffect(() => {
-    dispatch(getAllProducts());
-  }, [dispatch]);
+const CardContainer = ({ products }) => {
+    const dispatch = useDispatch();
 
-  return (
-    <div>
-      {products.map((product, index) => (
-        <Card
-          key={index}
-          name={product.name}
-          description={product.description}
-          price={product.price}
-          old_price={product.old_price}
-          image={product.image}
-          amount={product.amount}
-        />
-      ))}
-    </div>
-  );
+    useEffect(() => {
+        dispatch(getAllProducts());
+    }, [dispatch]);
+
+    return (
+        <div className={styles.cardContainer}>
+            {products.map((product, index) => (
+                <Card
+                    key={index}
+                    name={product.name}
+                    description={product.description}
+                    price={product.price}
+                    old_price={product.old_price}
+                    image={product.image}
+                    amount={product.amount}
+                />
+            ))}
+        </div>
+    );
 };
 
-export default Cards;
+export default CardContainer;
