4fb76e9b7744e54903540b851ed2a49a48a3d99b
alisonroa <roa.alisonn@gmail.com>
2023-10-01 18:11:12 -0300

    este si funca

--- Client/src/Components/CardContainer/CardContainer.jsx
+++ Client/src/Components/CardContainer/CardContainer.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getAllProducts } from '../../Redux/actions';
 import Card from '../Card/card';
 import styles from './CardContainer.module.css';
 
 const CardContainer = ({ products }) => {
     const dispatch = useDispatch();
-
+    const products = useSelector(state => state.products);
     useEffect(() => {
         dispatch(getAllProducts());
     }, [dispatch]);
