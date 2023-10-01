92864a9121029d22c3fb8828bcf3752fb95667bb
alisonroa <roa.alisonn@gmail.com>
2023-10-01 12:07:56 -0300

    borre html feo

--- Client/src/Views/CreateProduct/CreateProduct.jsx
+++ Client/src/Views/CreateProduct/CreateProduct.jsx
@@ -37,10 +37,6 @@ const Cloudinary = (props) => {
     return (
         <div>
             {console.log('este es el product', productPost)}
-            <h1>Formulario post producto</h1>
-            <h3>Los campos con '*' son obligatorios</h3>
-            <hr />
-            <h1>HAY QUE CONFIGURAR PARA QUE LAS CREDENCIALES NO QUEDEN EXPUESTAS</h1>
             <Container>
                 <FormGroup>
                     <Input
@@ -52,7 +48,7 @@ const Cloudinary = (props) => {
                     {loading ? (<h3>Cargando imagen...</h3>) : (<img src={productPost.image} style={{ width: '300px' }} />)}
                 </FormGroup>
             </Container>
-            <hr />
+          
             <label htmlFor="nameProduct">Nombre producto: {' '}</label>
             <input
                 type="text"
@@ -61,7 +57,7 @@ const Cloudinary = (props) => {
                 placeholder='Ej: Panaderia Pancho'
                 onChange={handleOnChange}
             />
-            <hr />
+            
             <label htmlFor="cantidad">Cantidad: {' '}</label>
             <input
                 type="text"
@@ -70,17 +66,17 @@ const Cloudinary = (props) => {
                 placeholder='Ej: 10'
                 onChange={handleOnChange}
             />
-            <hr />
+           
             <label htmlFor="descripcion">Descripcion: {' '}</label>
             <textarea
                 name="descripcion"
                 value={productPost.descripcion}
                 cols="30"
                 rows="6"
-                placeholder='Rica docena de pan de queso'
+                placeholder='Descripcion de tu producto'
                 onChange={handleOnChange}
             />
-            <hr />
+            
             <label htmlFor="precioViejo">Precio viejo: {' '}</label>
             <input
                 type="text"
@@ -89,7 +85,7 @@ const Cloudinary = (props) => {
                 placeholder='Ej: 800'
                 onChange={handleOnChange}
             />
-            <hr />
+           
             <label htmlFor="precioNuevo">Precio nuevo: {' '}</label>
             <input
                 type="text"
