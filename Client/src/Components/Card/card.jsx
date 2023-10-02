0ee282ab23a16092b4420e5ba02fac02450fff22
DSamuelEC <samueledcardenas@gmail.com>
2023-10-02 00:15:40 -0500

    arreglo imagen

--- Client/src/Components/Card/card.jsx
+++ Client/src/Components/Card/card.jsx
@@ -10,7 +10,7 @@ const Card = ({ name, description, price, old_price, image, amount, date }) => {
       <p>{description}</p>
       <p className={styles.price}>Price: {price}</p>
       <p className={styles.old_price}>Old Price: {old_price}</p>
-      <img src={image} alt={name} />
+      <img src='https://static.vecteezy.com/system/resources/thumbnails/007/126/723/small/chef-hat-line-art-icon-free-vector.jpg' alt={name} />
       <p className={styles.amount}>Amount: {amount}</p>
       <p className={styles.amount}>Fecha de posteo: {dateOk}</p>
 
