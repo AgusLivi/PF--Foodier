14ce4f2cbb71fa6b8d39cc8edf2b082618e8f32a
matiasvincent2002 <72351361+matiasvincent2002@users.noreply.github.com>
2023-10-01 22:11:13 -0300

    probando

--- Client/src/Views/Home/Home.jsx
+++ Client/src/Views/Home/Home.jsx
@@ -1,18 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import CardContainer from '../../Components/CardContainer/CardContainer.jsx';
-import Style from "./Home.module.css";
+import { useSelector, useDispatch } from 'react-redux'
+import { useState, useEffect } from 'react'
+import CardContainer from '../../Components/CardContainer/CardContainer.jsx'
+import Style from "./Home.module.css"
 import {
     categoriesFilter,
     selectedCategories,
-} from '../../Redux/actions';
+    getProductByName,
+    orderByp,
+    orderUpDown,
+} from '../../Redux/actions'
 
 const Home = () => {
-    const categories = useSelector(state => state.categories);
-    const products = useSelector(state => state.products);
-    const dispatch = useDispatch();
+   // global state 
+    const categories = useSelector(state => state.categories)
+    const products = useSelector(state => state.products)
+    const [reset, useReset] = useState(false)
+   
+    const [productName, setProductName] = useState('');
+    const [orderByy, setOrderByy] = useState('')
+    const [orderr, setOrderr] = useState('')
+    const dispatch = useDispatch()
 
-    // Estado para el formulario
+    useEffect(()=>{
+        dispatch(categoriesFilter())
+    }, [reset])
+    console.log(categories)
+
+
     const [formData, setFormData] = useState({
         name: '',
         categories: [],
@@ -22,41 +36,30 @@ const Home = () => {
         orderBy: 'name',
         order: 'asc'
     });
-
-    // Estado adicional para controlar el reinicio
-    const [reset, useReset] = useState(false);
-
-    // Efecto para cargar las categorías al montar el componente
-    useEffect(() => {
-        dispatch(categoriesFilter());
-    }, [reset]);
-
-    // Manejar cambios en el formulario
+    
     const handleInputChange = (event) => {
         const { name, value } = event.target;
         setFormData({ ...formData, [name]: value });
     };
-
-    // Manejar cambios en las categorías seleccionadas
+    
     const handleCategoryChange = (event) => {
         const { name, checked } = event.target;
         const { categories } = formData;
-
+    
         if (checked) {
-            setFormData({ ...formData, categories: [...categories, name] });
+          setFormData({ ...formData, categories: [...categories, name] });
         } else {
-            setFormData({ ...formData, categories: categories.filter((item) => item !== name) });
+          setFormData({ ...formData, categories: categories.filter((item) => item !== name) });
         }
     };
 
-    // Manejar el envío del formulario
     const handleSubmit = (event) => {
         event.preventDefault();
         const { name, categories, address, average_rating, payment, orderBy, order } = formData;
-
-        // Generar la cadena de consulta
+    
+        // generamos la cadena de consulta
         const queryParams = new URLSearchParams({
-            name,
+            name, // Usa productName en lugar de formData.name
             categories: categories.join(','),
             address,
             average_rating,
@@ -64,30 +67,34 @@ const Home = () => {
             orderBy,
             order,
         }).toString();
-
-        // Hacer dispatch con la cadena de consulta
+        console.log(queryParams);
+    
+        // hacer dispatch con la cadena de consulta
         dispatch(selectedCategories(queryParams));
     };
 
+
     return (
         <div>
-            <form onSubmit={handleSubmit}>
+    
+    <form onSubmit={handleSubmit}>
                 <input
                     type="text"
                     name="name"
                     placeholder="Nombre del producto"
                     value={formData.name}
-                    onChange={handleInputChange}
+                    onChange={(e) => setProductName(e.target.value)}
                 />
-
-                <select name='categories' value={formData.categories} onChange={handleCategoryChange} multiple>
-                    <option value="">Categoría</option>
-                    {categories.map((categoryItem) => (
-                        <option key={categoryItem} value={categoryItem}>
-                            {categoryItem}
-                        </option>
-                    ))}
-                </select>
+        
+                {/* mapeo categorías en checkboxes */}
+                <select name='categoriess' value={formData.categoriess} onChange={handleCategoryChange}>
+    <option value="">Categoría</option> {/* Opción predeterminada */}
+    {categories.map((categoryItem) => (
+        <option key={categoryItem} value={categoryItem}>
+            {categoryItem}
+        </option>
+    ))}
+</select>
 
                 <input
                     type="text"
@@ -96,60 +103,59 @@ const Home = () => {
                     value={formData.address}
                     onChange={handleInputChange}
                 />
-
-                <select
-                    name="average_rating"
-                    value={formData.average_rating}
-                    onChange={handleInputChange}
-                >
-                    <option value="">Todos</option>
-                    <option value="5">5 estrellas</option>
-                    <option value="4">4 estrellas</option>
-                    <option value="3">3 estrellas</option>
-                    <option value="2">2 estrellas</option>
-                    <option value="1">1 estrella</option>
-                </select>
-
-                <select
-                    name="payment"
-                    value={formData.payment}
-                    onChange={handleInputChange}
-                >
-                    <option value="">Cualquier forma de pago</option>
-                    <option value="Efectivo">Efectivo</option>
-                    <option value="Pago Online/Tarjeta">Pago Online/Tarjeta</option>
-                </select>
-
-                <div className={Style.filtro}>
-                    <label>Ordenar por:</label>
+        
                     <select
-                        name="orderBy"
-                        value={formData.orderBy}
+                        name="average_rating"
+                        value={formData.average_rating}
+                        onChange={handleInputChange}
+                        >
+                        <option value="">Todos</option>
+                        <option value="5">5 estrellas</option>
+                        <option value="4">4 estrellas</option>
+                        <option value="3">3 estrellas</option>
+                        <option value="2">2 estrellas</option>
+                        <option value="1">1 estrella</option>
+                    </select>
+        
+                    <select
+                        name="payment"
+                        value={formData.payment}
                         onChange={handleInputChange}
                     >
-                        <option value="name">Nombre</option>
-                        <option value="price">Precio</option>
+                        <option value="">Cualquier forma de pago</option>
+                        <option value="Efectivo">Efectivo</option>
+                        <option value="Pago Online/Tarjeta">Pago Online/Tarjeta</option>
                     </select>
-                </div>
+        
+                    <div className={Style.filtro}>
+                        <label>Ordenar por:</label>
+                        <select
+                            name="orderBy"
+                            value={formData.orderBy}
+                            onChange={(e) => setOrderByy(e.target.value)}
+                        >
+                            <option value="name">Nombre</option>
+                            <option value="price">Precio</option>
+                        </select>
+                        </div>
 
-                <div>
-                    <label>Orden:</label>
-                    <select
-                        name="order"
-                        value={formData.order}
-                        onChange={handleInputChange}
-                    >
-                        <option value="asc">Ascendente</option>
-                        <option value="desc">Descendente</option>
-                    </select>
-                </div>
-
+                        <div>
+                            <label>Orden:</label>
+                            <select
+                                name="order"
+                                value={formData.order}
+                                onChange={(e) => setOrderr(e.target.value)}
+                            >
+                                <option value="asc">Ascendente</option>
+                                <option value="desc">Descendente</option>
+                            </select>
+                        </div>
+        
                 <button type="submit">Filtrar</button>
             </form>
 
-            <CardContainer products={products} />
+             <CardContainer products={products}/>
         </div>
-    );
-};
-
-export default Home;
+    )
+}
+export default Home
