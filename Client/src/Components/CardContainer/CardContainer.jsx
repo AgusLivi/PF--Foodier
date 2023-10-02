a7b455f4fecc8d873a5f16cfa69c3588c09d4a1d
IVANH <ivan.hd.16@gmail.com>
2023-10-01 21:35:01 -0300

    card-date

--- Client/src/Components/CardContainer/CardContainer.jsx
+++ Client/src/Components/CardContainer/CardContainer.jsx
@@ -1,32 +1,34 @@
 import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getAllProducts } from '../../Redux/actions';
 import Card from '../Card/card';
-import styles from './CardContainer.module.css'; // Importa los estilos de módulo CSS
+import styles from './CardContainer.module.css';
 
 const CardContainer = () => {
-  const dispatch = useDispatch();
-  const products = useSelector(state => state.products);
-  
-  useEffect(() => {
-    dispatch(getAllProducts());
-  }, [dispatch]);
+    const dispatch = useDispatch();
+    const products = useSelector(state => state.products);
+    useEffect(() => {
+        dispatch(getAllProducts());
+    }, [dispatch]);
 
-  return (
-    <div className={styles.cardContainer}>
-      {products.map((product, index) => (
-        <Card
-          key={index}
-          name={product.name}
-          description={product.description}
-          price={product.price}
-          old_price={product.old_price}
-          image={product.image}
-          amount={product.amount}
-        />
-      ))}
-    </div>
-  );
+    return (
+        <div className={styles.cardContainer}>
+            {products.map((product, index) => (
+                <Card
+                    key={index}
+                    name={product.name}
+                    description={product.description}
+                    price={product.price}
+                    old_price={product.old_price}
+                    image={product.image}
+                    amount={product.amount}
+                    date={product.createdAt}
+                />
+            ))}
+        </div>
+    );
 };
 
 export default CardContainer;
+
+
