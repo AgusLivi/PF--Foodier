566cfaa0c8b95e52d49591e8f2792f7a27b75a9f
matiasvincent2002 <72351361+matiasvincent2002@users.noreply.github.com>
2023-10-02 08:59:17 -0300

    el cambio de checkbox

--- Client/src/Views/Home/Home.jsx
+++ Client/src/Views/Home/Home.jsx
@@ -1,84 +1,86 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import CardContainer from '../../Components/CardContainer/CardContainer.jsx';
-import Style from './Home.module.css';
+import { useSelector, useDispatch } from 'react-redux'
+import { useState, useEffect } from 'react'
+import CardContainer from '../../Components/CardContainer/CardContainer.jsx'
+import Style from "./Home.module.css"
 import {
-  categoriesFilter,
-  selectedCategories,
-  getProductByName,
-  orderByp,
-  orderUpDown,
-} from '../../Redux/actions';
+    categoriesFilter,
+    selectedCategories,
+    getProductByName,
+    orderByp,
+    orderUpDown,
+} from '../../Redux/actions'
 
 const Home = () => {
-  // global state
-  const categories = useSelector((state) => state.categories);
-  const products = useSelector((state) => state.products);
+   // global state 
+    const categories = useSelector(state => state.categories)
+    const products = useSelector(state => state.products)
+    const [reset, useReset] = useState(false)
+   
+    const [productName, setProductName] = useState('');
+    const [orderByy, setOrderByy] = useState('')
+    const [orderr, setOrderr] = useState('')
+    const dispatch = useDispatch()
 
-  const dispatch = useDispatch();
+    useEffect(()=>{
+        dispatch(categoriesFilter())
+    }, [reset])
+    console.log(categories)
 
-  useEffect(() => {
-    dispatch(categoriesFilter());
-  }, []);
 
-  const [formData, setFormData] = useState({
-    name: '',
-    categories: [],
-    address: '',
-    average_rating: '',
-    payment: '',
-    orderBy: 'name',
-    order: 'asc',
-  });
+    const [formData, setFormData] = useState({
+        name: '',
+        categories: [],
+        address: '',
+        average_rating: '',
+        payment: '',
+        orderBy: 'name',
+        order: 'asc'
+    });
+    
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({ ...formData, [name]: value });
+    };
+    
+    const handleCategoryChange = (event) => {
+        const { name, checked } = event.target;
+        const { categories } = formData;
+    
+        if (checked) {
+          setFormData({ ...formData, categories: [...categories, name] });
+        } else {
+          setFormData({ ...formData, categories: categories.filter((item) => item !== name) });
+        }
+    };
 
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setFormData({ ...formData, [name]: value });
-  };
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const { name, categories, address, average_rating, payment, orderBy, order } = formData;
+    
+        // generamos la cadena de consulta
+        const queryParams = new URLSearchParams({
+            name, // Usa productName en lugar de formData.name
+            categories: categories.join(','),
+            address,
+            average_rating,
+            payment,
+            orderBy,
+            order,
+        }).toString();
+        console.log(queryParams);
+    
+        // hacer dispatch con la cadena de consulta
+        dispatch(selectedCategories(queryParams));
+    };
+    const [isModalVisible, setIsModalVisible] = useState(false);
 
-  const handleCategoryChange = (event) => {
-    const { name, checked } = event.target;
-    const { categories } = formData;
+    // Función para mostrar u ocultar el modal
+    const toggleModal = () => {
+      setIsModalVisible(!isModalVisible);
+    };
 
-    if (checked) {
-      setFormData({ ...formData, categories: [...categories, name] });
-    } else {
-      setFormData({
-        ...formData,
-        categories: categories.filter((item) => item !== name),
-      });
-    }
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const { name, categories, address, average_rating, payment, orderBy, order } = formData;
-
-    const queryParams = new URLSearchParams({
-      name,
-      categories: categories.join(','),
-      address,
-      average_rating,
-      payment,
-      orderBy,
-      order,
-    }).toString();
-
-    dispatch(selectedCategories(queryParams));
-  };
-
-  // Estado para controlar la visibilidad del modal
-  const [isModalVisible, setIsModalVisible] = useState(false);
-
-  // Función para mostrar u ocultar el modal
-  const toggleModal = () => {
-    setIsModalVisible(!isModalVisible);
-  };
-
-  return (
-    <div>
-  
-      {/* Modal */}
+    return (
+        <div>
       <div className={`${Style.checkbox} ${isModalVisible ? Style.modalVisible : ''}`}>
         <button name="categoria" id="showHideButton" onClick={toggleModal}>
           X
@@ -95,64 +97,79 @@ const Home = () => {
             {categoryItem}
           </label>
         ))}
-      </div>
 
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="name"
-          placeholder="Nombre del producto"
-          value={formData.name}
-          onChange={handleInputChange}
-        />
+            </div>
+    <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="Nombre del producto"
+                    value={formData.name}
+                    onChange={(e) => setProductName(e.target.value)}
+                />
+        
+          {/* Modal */}
+    
+                <input
+                    type="text"
+                    name="address"
+                    placeholder="Ubicación"
+                    value={formData.address}
+                    onChange={handleInputChange}
+                />
+            <button onClick={toggleModal} className={Style.categoria}>Categorias </button>
+                    <select
+                        name="average_rating"
+                        value={formData.average_rating}
+                        onChange={handleInputChange}
+                        >
+                        <option value="">Todos</option>
+                        <option value="5">5 estrellas</option>
+                        <option value="4">4 estrellas</option>
+                        <option value="3">3 estrellas</option>
+                        <option value="2">2 estrellas</option>
+                        <option value="1">1 estrella</option>
+                    </select>
+        
+                    <select
+                        name="payment"
+                        value={formData.payment}
+                        onChange={handleInputChange}
+                    >
+                        <option value="">Cualquier forma de pago</option>
+                        <option value="Efectivo">Efectivo</option>
+                        <option value="Pago Online/Tarjeta">Pago Online/Tarjeta</option>
+                    </select>
+        
+                    <div className={Style.filtro}>
+                        <label>Ordenar por:</label>
+                        <select
+                            name="orderBy"
+                            value={formData.orderBy}
+                            onChange={(e) => setOrderByy(e.target.value)}
+                        >
+                            <option value="name">Nombre</option>
+                            <option value="price">Precio</option>
+                        </select>
+                        </div>
 
-        <input
-          type="text"
-          name="address"
-          placeholder="Ubicación"
-          value={formData.address}
-          onChange={handleInputChange}
-        />
-    {/* Botón para mostrar/ocultar el modal */}
-    <button onClick={toggleModal} className={Style.categoria}>Categorias </button>
+                        <div>
+                            <label>Orden:</label>
+                            <select
+                                name="order"
+                                value={formData.order}
+                                onChange={(e) => setOrderr(e.target.value)}
+                            >
+                                <option value="asc">Ascendente</option>
+                                <option value="desc">Descendente</option>
+                            </select>
+                        </div>
+        
+                <button type="submit">Filtrar</button>
+            </form>
 
-        <select name="average_rating" value={formData.average_rating} onChange={handleInputChange}>
-          <option value="">Todos</option>
-          <option value="5">5 estrellas</option>
-          <option value="4">4 estrellas</option>
-          <option value="3">3 estrellas</option>
-          <option value="2">2 estrellas</option>
-          <option value="1">1 estrella</option>
-        </select>
-
-        <select name="payment" value={formData.payment} onChange={handleInputChange}>
-          <option value="">Cualquier forma de pago</option>
-          <option value="Efectivo">Efectivo</option>
-          <option value="Pago Online/Tarjeta">Pago Online/Tarjeta</option>
-        </select>
-
-        <div className={Style.filtro}>
-          <label>Ordenar por:</label>
-          <select name="orderBy" value={formData.orderBy} onChange={(e) => setOrderByy(e.target.value)}>
-            <option value="name">Nombre</option>
-            <option value="price">Precio</option>
-          </select>
+             <CardContainer products={products}/>
         </div>
-
-        <div>
-          <label>Orden:</label>
-          <select name="order" value={formData.order} onChange={(e) => setOrderr(e.target.value)}>
-            <option value="asc">Ascendente</option>
-            <option value="desc">Descendente</option>
-          </select>
-        </div>
-
-        <button type="submit">Filtrar</button>
-      </form>
-
-      <CardContainer products={products} />
-    </div>
-  );
-};
-
-export default Home;
+    )
+}
+export default Home
