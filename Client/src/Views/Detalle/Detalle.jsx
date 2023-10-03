9cc9abbf90480806e9f08a2333b7eebcde3d339f
alisonroa <roa.alisonn@gmail.com>
2023-10-03 12:49:31 -0300

    comentarios

--- Client/src/Views/Detalle/Detalle.jsx
+++ Client/src/Views/Detalle/Detalle.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 
 const Detalle = ({ name, description, price, old_price, image, amount, date, product_ID, rating, categories }) => {
+    
   return (
     <div key={product_ID}>
       <h2>{name}</h2>
