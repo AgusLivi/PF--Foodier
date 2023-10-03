0fd30a34db699e940c489d058b3a6615bba4df2a
alisonroa <roa.alisonn@gmail.com>
2023-10-03 10:49:21 -0300

    validacion de formulario de usuario

--- Client/src/Components/FormUser/FormUser.jsx
+++ Client/src/Components/FormUser/FormUser.jsx
@@ -4,8 +4,38 @@ import { Link } from 'react-router-dom';
 
 const FormLogin = () => {
   const submitForm = (values) => {
-    // Aquí puedes realizar la lógica de envío del formulario, como enviar una solicitud a un servidor.
+
     console.log(values);
+  };
+
+  const validateForm = (values) => {
+    const errors = {};
+
+    // Validación para el campo nombre
+    if (!values.nombre) {
+      errors.nombre = 'El nombre es obligatorio';
+    }
+
+    // Validación para el campo usuario
+    if (!values.usuario) {
+      errors.usuario = 'El usuario es obligatorio';
+    }
+
+    // Validación para el campo email
+    if (!values.email) {
+      errors.email = 'El correo es obligatorio';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+      errors.email = 'Formato de correo electrónico inválido';
+    }
+
+    // Validación para el campo contraseña
+    if (!values.password) {
+      errors.password = 'La contraseña es obligatoria';
+    } else if (!/(?=.*[A-Z])(?=.*\d)/.test(values.password)) {
+      errors.password = 'La contraseña debe contener al menos una mayúscula y un número';
+    }
+
+    return errors;
   };
 
   const formik = useFormik({
@@ -16,20 +46,20 @@ const FormLogin = () => {
       password: '',
     },
     onSubmit: submitForm,
-    // Puedes agregar validaciones aquí utilizando la propiedad "validate" si es necesario.
+    validate: validateForm, // Agregamos la función de validación aquí
   });
 
   return (
     <div>
       <form onSubmit={formik.handleSubmit}>
-
-      <input
+        <input
           type='text'
           placeholder='Nombre'
           name='nombre'
           onChange={formik.handleChange}
           value={formik.values.nombre}
         />
+        {formik.errors.nombre && <div className='error'>{formik.errors.nombre}</div>}
 
         <input
           type='text'
@@ -38,6 +68,7 @@ const FormLogin = () => {
           onChange={formik.handleChange}
           value={formik.values.usuario}
         />
+        {formik.errors.usuario && <div className='error'>{formik.errors.usuario}</div>}
 
         <input
           type='email'
@@ -46,6 +77,8 @@ const FormLogin = () => {
           onChange={formik.handleChange}
           value={formik.values.email}
         />
+        {formik.errors.email && <div className='error'>{formik.errors.email}</div>}
+
         <input
           type='password'
           placeholder='Contraseña'
@@ -53,8 +86,10 @@ const FormLogin = () => {
           onChange={formik.handleChange}
           value={formik.values.password}
         />
+        {formik.errors.password && <div className='error'>{formik.errors.password}</div>}
+
         <Link to={`/home`}>
-        <button type='submit'>Registrarse</button>
+          <button type='submit'>Registrarse</button>
         </Link>
       </form>
     </div>
