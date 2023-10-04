d90c8a2483b0a3e99aa55601ff5d535e0887c9c6
Ayrton H. Torres <ayrton.torres.1995@gmail.com>
2023-10-04 15:18:42 -0300

    aun no termino

--- Client/src/Redux/actions.js
+++ Client/src/Redux/actions.js
@@ -1,38 +1,36 @@
 import axios from 'axios';
 import {
-    CREATE_PRODUCT,
-    GET_ALL_FAV,
-    DELETE_PRODUCT,
-    POST_FAVORITES,
-    CREATE_USER,
-    GET_SELLER_FAV,
-    GET_CATEGORIES,
-    CREATE_POST,
-    CREATE_PAYMENT,
-    GET_USER_BY_ID,
-    GET_SELLER_BY_ID,
-    GET_ALL_SELLERS,
-    DELETE_SELLER,
-    UPDATE_SELLER,
-    CREATE_SELLER,
-    GET_ALL_USER,
-    DELETE_USER,
-    UPDATE_USER,
-    GET_POST_BY_ID,
-    GET_POST,
+    
+    //product actionTypes:
     GET_PRODUCTS,
+    SEARCH_BY_ID,
+    CLEAN_DETAIL,
 
-    GET_PRODUCT_BY_ID,
+    //user actionTypes:
+    GET_USER_BY_ID,
+    GET_ALL_USER,
+
+    
+    //post actionTypes:
+    GET_POST_BY_ID,
+    CREATE_POST,
+    GET_POST,
+
+    //Seller actionTypes:
+    GET_SELLER_FAV,
+    GET_ALL_SELLERS,
+    GET_SELLER_BY_ID,
+    POST_FAVORITES,
+    GET_ALL_FAV,
+    CREATE_PAYMENT,
+    
+    //location actionTypes:
     MUNICIPIOS,
     PROVINCIAS,
     LOCALIDADES,
 
-   
-    SEARCH_BY_ID,
-    CLEAN_DETAIL
-
-
-    //
+    
+    GET_CATEGORIES,
 } from './actionsType'
 
 const endPoint = 'http://localhost:3001'  //definir rutas del back
@@ -43,34 +41,6 @@ export const getAllFav = (id) => {
             const { data } = await axios(`${endPoint}/get-favorites/${id}`) //definir despues como pusieron la ruta en el back
             return dispatch({
                 type: GET_ALL_FAV,
-                payload: data
-            })
-        } catch (error) {
-            alert(error.message)
-        }
-    }
-}
-
-/*export const getById = (id) => {
-    return async (dispatch) => {
-        try {
-            const { data } = await axios.get(`${endPoint}/products/${id}`) //definir las rutas del back
-            return dispatch({
-                type: SEARCH_BY_ID,
-                payload: data
-            })
-        } catch (error) {
-            alert(error.message)
-        }
-    }
-} YA HAY RUTA DE TRAER POR ID*/
-
-export const deleteProduct = (id) => {
-    return async (dispatch) => {
-        try {
-            const { data } = await axios.delete(`${endPoint}/products/${id}`) //definir las rutas del back
-            return dispatch({
-                type: DELETE_PRODUCT,
                 payload: data
             })
         } catch (error) {
@@ -93,19 +63,7 @@ export const postFav = (dataForm) => {
     }
 }
 
-export const createUser = (userData) => {
-    return async (dispatch) => {
-        try {
-            const { data } = await axios.post(`${endPoint}/users`, userData)
-            return dispatch({
-                type: CREATE_USER,
-                payload: data,
-            })
-        } catch (error) {
-            alert(error.message)
-        }
-    }
-}
+
 
 export const getSellerFav = (id) => {
     return async (dispatch) => {
@@ -122,13 +80,10 @@ export const getSellerFav = (id) => {
 }
 
 export const createPayment = (pay) => {
-    return async (dispatch) => {
+    return async () => {
         try {
             const { data } = await axios.post(`${endPoint}/payments`, pay)
-            return dispatch({
-                type: CREATE_PAYMENT,
-                payload: data
-            })
+            alert(`orden ${data} creado`)
         } catch (error) {
             alert(error.message)
         }
@@ -192,13 +147,10 @@ export const getPostById = (id) => {
 }
 
 export const updateUser = (id, updatedUserData) => {
-    return async (dispatch) => {
+    return async () => {
         try {
             const { data } = await axios.put(`/users/${id}`, updatedUserData);
-            return dispatch({
-                type: UPDATE_USER,
-                payload: data
-            });
+            alert(`Usuario ${data.name} actualizado correctamente`)
         } catch (error) {
             alert(error.message);
         }
@@ -206,13 +158,10 @@ export const updateUser = (id, updatedUserData) => {
 }
 
 export const deleteUser = (id) => {
-    return async (dispatch) => {
+    return async () => {
         try {
             const { data } = await axios.delete(`${endPoint}/users/${id}`) //definir las rutas del back
-            return dispatch({
-                type: DELETE_USER,
-                payload: data
-            })
+            alert(`Usuario ${data.name} borrado correctamente`)
         } catch (error) {
             alert(error.message)
         }
@@ -248,13 +197,10 @@ export const getUserById = (id) => {
 }
 
 export const createSeller = (sellerData) => {
-    return async (dispatch) => {
+    return async () => {
         try {
             const { data } = await axios.post(`${endPoint}/sellers`, sellerData)
-            return dispatch({
-                type: CREATE_SELLER,
-                payload: data
-            })
+            alert(`vendedor ${data.name} creado correctamente`)
         } catch (error) {
             alert(error.message)
         }
@@ -262,13 +208,10 @@ export const createSeller = (sellerData) => {
 }
 
 export const updateSeller = (id, updatedSellerData) => {
-    return async (dispatch) => {
+    return async () => {
         try {
             const { data } = await axios.put(`/sellers/${id}`, updatedSellerData);
-            return dispatch({
-                type: UPDATE_SELLER,
-                payload: data
-            });
+            alert(`vendedor ${data.name} actualizado correctamente`)
         } catch (error) {
             alert(error.message);
         }
@@ -276,13 +219,10 @@ export const updateSeller = (id, updatedSellerData) => {
 }
 
 export const deleteSeller = (id) => {
-    return async (dispatch) => {
+    return async () => {
         try {
             const { data } = await axios.delete(`${endPoint}/sellers/${id}`) //definir las rutas del back
-            return dispatch({
-                type: DELETE_SELLER,
-                payload: data
-            })
+            alert(`vendedor ${data.name} borrado correctamente`)
         } catch (error) {
             alert(error.message)
         }
@@ -330,13 +270,24 @@ export const getProducts = (querys) => {
     }
 }
 
+export const deleteProduct = (id) => {
+    return async (dispatch) => {
+        try {
+            const { data } = await axios.delete(`${endPoint}/products/${id}`) //definir las rutas del back
+            alert(`${data.name} fue borrado correctamente`)
+        } catch (error) {
+            alert(error.message)
+        }
+    }
+}
+
 export const getProductById = (product_ID) => {
     return async function (dispatch) {
         console.log('Fetching product by ID:', product_ID);
-        const productById = await axios(`${endPoint}/products/${product_ID}`)
+        const { data } = await axios(`${endPoint}/products/${product_ID}`)
         return dispatch({
             type: SEARCH_BY_ID,
-            payload: productById.data
+            payload: data
         })
     }
 };
@@ -349,18 +300,17 @@ export const cleanDetail = () => {
 
 export const createProduct = (formmData, id) => {
     console.log('form data: ', formmData);
-    return async (dispatch) => {
+    return async () => {
         try {
             const { data } = await axios.post(`${endPoint}/products/${id}`, formmData); // despues de la barra tengo que poner la ruta que definieron en el back
-            return dispatch({
-                type: CREATE_PRODUCT,
-                payload: data
-            })
+            alert (`${data.name} fue creado correctamente`)
         } catch (error) {
             console.error(error.message)
         }
     }
 }
+
+// Location actions
 
 export const locationProvincia = () => {
     return async (dispatch) => {
@@ -403,3 +353,18 @@ export const locationLocalidad = (muniId) => {
         }
     };
 };
+
+// User actions
+
+export const createUser = (userData) => {
+    return async () => {
+        try {
+            const { data } = await axios.post(`${endPoint}/users`, userData)
+            alert(`Usuario ${data.name} creado correctamente`)
+
+        } catch (error) {
+            alert(error.message)
+        }
+    }
+}
+
--- Client/src/Redux/reducer.js
+++ Client/src/Redux/reducer.js
@@ -1,229 +1,146 @@
-
 import {
-    CREATE_PRODUCT,
-    GET_ALL_FAV,
-    DELETE_PRODUCT,
-    POST_FAVORITES,
-    CREATE_USER,
-    GET_SELLER_FAV,
-    CREATE_POST,
-    CREATE_PAYMENT,
+    
+    //product actionTypes:
+    GET_PRODUCTS,
+    SEARCH_BY_ID,
+    CLEAN_DETAIL,
+
+    //user actionTypes:
     GET_USER_BY_ID,
-    GET_SELLER_BY_ID,
-    GET_ALL_SELLERS,
-    DELETE_SELLER,
-    UPDATE_SELLER,
-    CREATE_SELLER,
     GET_ALL_USER,
-    DELETE_USER,
-    UPDATE_USER,
+    
+    //post actionTypes:
     GET_POST_BY_ID,
     GET_POST,
-    GET_PRODUCTS,
-    GET_PRODUCT_BY_ID,
-    GET_CATEGORIES,
 
-    PROVINCIAS,
+    //Seller actionTypes:
+    GET_SELLER_FAV,
+    GET_ALL_SELLERS,
+    GET_SELLER_BY_ID,
+    
+    GET_ALL_FAV,
+    CREATE_PAYMENT,
+    
+    //location actionTypes:
     MUNICIPIOS,
+    PROVINCIAS,
     LOCALIDADES,
 
-    SEARCH_BY_ID,
-    CLEAN_DETAIL
+    
+    GET_CATEGORIES,
+} from "./actionsType";
 
-} from './actionsType'
+const initialState = {
+  products: [],
+  productsCopy: [],
+  productDetail:{},
+  sellersFav: [],
+  users: [],
+  getUserById: [],
+  sellers: [],
+  getSellerById: [],
+  categories: [],
+  paymentMethods: [],
+  post: [],
+  allFavoritesSeller: [],
+  provincias: [],
+  municipios: [],
+  localidades: [],
+  //tengo q agregar mas
+};
 
+const reducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    //pruducts CASEs
+    case GET_PRODUCTS:
 
+      return {
+        ...state,
+        products: payload,
+        productsCopy: payload,
+      };
 
+    case GET_SELLER_FAV:
+      return {
+        ...state,
+        sellersFav: payload,
+      };
 
-    const initialState = { 
-        products: [],
-        productsCopy: [],
-        createdProduct: [],
-        sellersFav: [],
-        productsByName: [],
-        postedFiltereds: [],
-        createdUser: [],
-        orderBy: [],
-        selectedOrderBy: [],
-        selectedOrder: [],
-        createdPost: [],
-        createPayment: [],
-        averageRating: [],
-        address: [],
-        users: [],
-        getUserById: [],
-        sellers: [],
-        getSellerById: [],
-        categories: [],
-        paymentMethods: [],
-        post: [],
-        allFavoritesSeller: [],
-        createdUser: [],
-        createdSellers: [],
-        provincias: [],
-        municipios: [],
-        localidades: []
-        //tengo q agregar mas
-    };
+    case GET_CATEGORIES:
+      return {
+        ...state,
+        categories: payload,
+      };
 
-    const reducer = (state = initialState, { type, payload }) => {
+    case GET_USER_BY_ID:
+      return {
+        ...state,
+        getUserById: payload,
+      };
+    case GET_SELLER_BY_ID:
+      return {
+        ...state,
+        getSellerById: payload,
+      };
+    case GET_ALL_SELLERS:
+      return {
+        ...state,
+        sellers: payload,
+      };
+    case GET_ALL_USER:
+      return {
+        ...state,
+        users: payload,
+      };
 
-    switch(type){
+    case GET_POST:
+      return {
+        ...state,
+        post: payload,
+      };
+    case GET_POST_BY_ID:
+      return {
+        ...state,
+        post: payload,
+      };
+    case GET_ALL_FAV:
+      return {
+        ...state,
+        allFavoritesSeller: payload,
+      };
+    case PROVINCIAS:
+      return {
+        ...state,
+        provincias: payload,
+      };
 
+    case MUNICIPIOS:
+      return {
+        ...state,
+        municipios: payload,
+      };
 
-            case GET_PRODUCTS:
-                return {
-                    ...state,
-                    products: payload,
-                    productsCopy: payload,
-                };
+    case LOCALIDADES:
+      return {
+        ...state,
+        localidades: payload,
+      };
 
-            case GET_PRODUCT_BY_ID:
-                return {
-                    ...state,
-                    products: payload,
-                };
+    case SEARCH_BY_ID:
+      return {
+        ...state,
+        productDetail: payload,
+      };
 
-            case CREATE_PRODUCT:
-                return {
-                    ...state,
-                    createdProduct: [...state.createdProduct, payload]
-                }
+    case CLEAN_DETAIL:
+      return {
+        ...state,
+        productDetail: {},
+      };
 
-            case DELETE_PRODUCT:
-                return {
-                    ...state,
-                    products: payload,
-                    productsCopy: payload,
-                };
-            case POST_FAVORITES:
-                return {
-                    ...state,
-                    sellersFav: payload,
-                }
-            case CREATE_USER: 
-                return {
-                    ...state,
-                    createdUser: [...state.createdUser, payload]
-                }
-
-            case GET_SELLER_FAV: 
-                return {
-                    ...state,
-                    sellersFav: payload
-                }
-            case GET_CATEGORIES:
-                return {
-                    ...state,
-                    categories: payload
-                }
-
-
-            case CREATE_POST:
-                return {
-                    ...state,
-                    createdPost: [...state.createdPost, payload]
-                }
-            case CREATE_PAYMENT: 
-                return {
-                    ...state,
-                    createPayment:[...state.createPayment, payload]
-                }
-            case GET_USER_BY_ID:
-                return {
-                    ...state,
-                    getUserById: payload
-                }
-            case GET_SELLER_BY_ID:
-                return {
-                    ...state,
-                    getSellerById: payload
-                }
-            case GET_ALL_SELLERS: 
-                return {
-                    ...state,
-                    sellers: payload
-                }
-            case GET_ALL_USER:
-                return {
-                    ...state,
-                    users: payload
-                }
-            case DELETE_USER:
-                return {
-                    ...state,
-                    createdUsers: payload
-                }
-            case DELETE_SELLER:
-                return {
-                    ...state,
-                    createdSellers: payload
-                }
-            case UPDATE_SELLER: 
-                return {
-                    ...state,
-                    createdSellers: payload
-                }
-            case UPDATE_USER: 
-                return {
-                    ...state,
-                    createUser: payload
-                }
-            case CREATE_SELLER: 
-                return {
-                    ...state,
-                    createdSellers: payload
-                }
-            case GET_POST:
-                return {
-                    ...state,
-                    post: payload
-                }
-            case GET_POST_BY_ID:
-                return {
-                    ...state,
-                    post: payload
-                }
-            case GET_ALL_FAV:
-                return {
-                    ...state,
-                    allFavoritesSeller: payload,
-                }
-            case PROVINCIAS:
-                return {
-                    ...state,
-                    provincias: payload
-                }
-
-            case MUNICIPIOS:
-                return {
-                    ...state,
-                    municipios: payload
-                }
-
-            case LOCALIDADES:
-                return {
-                    ...state,
-                    localidades: payload
-                }
-
-
-        case SEARCH_BY_ID:
-            return {
-                ...state,
-                productDetail: payload
-            }
-
-        case CLEAN_DETAIL:
-            return {
-                ...state,
-                productDetail: {}
-            }
-
-        default:
-            return state;
-    }
+    default:
+      return state;
+  }
 };
 
 export default reducer;
-
