95d1148daebdf9bd330e75450354fbf1f3d3176c
Ayrton H. Torres <ayrton.torres.1995@gmail.com>
2023-10-04 11:03:05 -0300

    10 usuraios creados

---
+++ usuariosHC.js
@@ -0,0 +1,62 @@
+module.exports = [
+    {
+        "name": "Juan Pérez",
+        "email": "juan.perez@gmail.com",
+        "password": "1235456",
+        "location": "Buenos Aires, Moreno, La Reja"
+    },
+    {
+        "name": "María Rodríguez",
+        "email": "maria.rodriguez@gmail.com",
+        "password": "1235456",
+        "location": "Córdoba, Córdoba, Centro"
+    },
+    {
+        "name": "Luis González",
+        "email": "luis.gonzalez@gmail.com",
+        "password": "1235456",
+        "location": "Santa Fe, Rosario, Pichincha"
+    },
+    {
+        "name": "Ana Martínez",
+        "email": "ana.martinez@gmail.com",
+        "password": "1235456",
+        "location": "Mendoza, Mendoza, Ciudad"
+    },
+    {
+        "name": "Jorge López",
+        "email": "jorge.lopez@gmail.com",
+        "password": "1235456",
+        "location": "Tucumán, San Miguel de Tucumán, Yerba Buena"
+    },
+    {
+        "name": "María Fernández",
+        "email": "maria.fernandez@gmail.com",
+        "password": "1235456",
+        "location": "Entre Ríos, Paraná, Centro"
+    },
+    {
+        "name": "Carlos Pérez",
+        "email": "carlos.perez@gmail.com",
+        "password": "1235456",
+        "location": "Salta, Salta, Centro"
+    },
+    {
+        "name": "Sofía González",
+        "email": "sofia.gonzalez@gmail.com",
+        "password": "1235456",
+        "location": "Chaco, Resistencia, Villa Centenario"
+    },
+    {
+        "name": "Pedro Martínez",
+        "email": "pedro.martinez@gmail.com",
+        "password": "1235456",
+        "location": "Corrientes, Corrientes, Centro"
+    },
+    {
+        "name": "Laura Rodríguez",
+        "email": "laura.rodriguez@gmail.com",
+        "password": "1235456",
+        "location": "Misiones, Posadas, Centro"
+    }
+]
