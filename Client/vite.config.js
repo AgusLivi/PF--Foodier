619c6dd169a632bba28fc961f5098827a1e8316f
Ayrton H. Torres <ayrton.torres.1995@gmail.com>
2023-10-10 17:01:24 -0300

    front configurado para deploy

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,5 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// import axios from 'axios';
+
+// // axios.defaults.baseURL = "http://localhost:3001";
+// axios.defaults.baseURL = "https://foodier-back-deploy.onrender.com/";
+
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
