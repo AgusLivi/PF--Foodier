d658ada6cda350e71696ec81eb54f933dbb3374b
Ayrton H. Torres <100615423+hatfeels@users.noreply.github.com>
2023-10-12 12:02:04 -0300

    Update main.jsx

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -6,8 +6,8 @@ import {Provider} from 'react-redux'
 import store from './Redux/store.js'
 import axios from 'axios'
 
-axios.defaults.baseURL = "http://localhost:3001";
-// axios.defaults.baseURL = "https://foodier-back-deploy.onrender.com/";
+//axios.defaults.baseURL = "http://localhost:3001";
+ axios.defaults.baseURL = "https://foodier-back-deploy.onrender.com/";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
