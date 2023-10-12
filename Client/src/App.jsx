771c7627342392f104a72b018604bda7c9caa9b4
alisonroa <roa.alisonn@gmail.com>
2023-10-12 19:26:55 -0300

    contador de carrito, falta trabajo

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,8 @@
-//App.jsx
+import React from 'react';
 import { Routes, Route, useLocation } from "react-router-dom";
+import NavBar from './Components/NavBar/NavBar';
+import Detalle from './Views/Detalle/Detalle';
 import PerfilUsuario from './Views/PerfilUsuario/PerfilUsuario'
-import NavBar from './Components/NavBar/NavBar'
 import Login from './Components/Login/Login';
 import Landing from './Views/Landing/Landing'
 import FormUser from "./Components/FormUser/FormUser";
@@ -11,7 +12,6 @@ import Home from "./Views/Home/Home"
 import Favoritos from "./Views/Favoritos/Favoritos";
 import FormComercio from './Components/FormComercio/FormComercio';
 import CreateProduct from './Views/CreateProduct/CreateProduct';
-import Detalle from './Views/Detalle/Detalle';
 import SignUp from "./Components/SignUp/SignUp";
 import Payment from "./Components/Payment/Payment";
 import SuccessPage from "./Components/Payment/SuccessPage";
@@ -21,13 +21,13 @@ import ShoppingCart from './Components/ShoppingCart/ShoppingCart';
 import Reserva from "./Components/Reserva/Reserva";
 import Terminos from "./Views/TerminosYCondiciones/Terminos";
 import Politica from "./Views/PoliticaDePrivacidad/Politica";
-
+import { CartProvider } from './Utils/CartContext';
 
 const App = () => {
   const location = useLocation();
   return (
     <div className="App">
-
+      <CartProvider>
       {location.pathname !== "/" 
       && location.pathname !== "/login" 
       && location.pathname !== "/formcomercio" 
@@ -35,28 +35,29 @@ const App = () => {
       && location.pathname !== "/userlogin" 
       && <NavBar />}
 
-      <Routes>
-        <Route path='/' element={<Landing />} />
-        <Route path='/profile' element={<PerfilUsuario />} /> {/*vacio*/}
-        <Route path="/home" element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/business' element={<PerfilNegocio />} /> {/*vacio*/}
-        <Route path='/formcomercio' element={<FormComercio />} />
-        <Route path="/userlogin" element={<SignUp/>}/> 
-        <Route path='/formuser' element={<FormUser />} />
-        <Route path='/favoritos' element={<Favoritos />} />
-        <Route path='/create' element={<CreateProduct />} />
-        <Route path='/products/:product_ID' element={<Detalle/>} />
-        <Route path='/payments/:monto/:descripcion' element={<Payment/>}/>
-        <Route path="/payments/success" element={<SuccessPage/>} />
-        <Route path="/payments/pending" element={<PendingPage/>} />
-        <Route path="/payments/failure" element={<FailurePage/>} />
-        <Route path='/carrito' element={<ShoppingCart />} />
-        <Route path='/reserva' element={<Reserva />} />
-        <Route path='/terminos-y-condiciones' element={<Terminos />} />
-        <Route path='/politica-de-privacidad' element={<Politica />} />
-
-      </Routes>
+    
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='/profile' element={<PerfilUsuario />} />
+          <Route path="/home" element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/business' element={<PerfilNegocio />} />
+          <Route path='/formcomercio' element={<FormComercio />} />
+          <Route path="/userlogin" element={<SignUp/>}/>
+          <Route path='/formuser' element={<FormUser />} />
+          <Route path='/favoritos' element={<Favoritos />} />
+          <Route path='/create' element={<CreateProduct />} />
+          <Route path='/products/:product_ID' element={<Detalle/>} />
+          <Route path='/payments/:monto/:descripcion' element={<Payment/>} />
+          <Route path="/payments/success" element={<SuccessPage/>} />
+          <Route path="/payments/pending" element={<PendingPage/>} />
+          <Route path="/payments/failure" element={<FailurePage/>} />
+          <Route path='/carrito' element={<ShoppingCart />} />
+          <Route path='/reserva' element={<Reserva />} />
+          <Route path='/terminos-y-condiciones' element={<Terminos />} />
+          <Route path='/politica-de-privacidad' element={<Politica />} />
+        </Routes>
+      </CartProvider>
 
       {location.pathname !== "/"  
       && location.pathname !== "/login" 
@@ -64,9 +65,8 @@ const App = () => {
       && location.pathname !== "/formuser" 
       && location.pathname !== "/userlogin" 
       && <Footer />}
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
