5ce39a8bd17c5b20dc43fa52d338fa37272065c4
alisonroa <roa.alisonn@gmail.com>
2023-10-13 18:53:32 -0300

    agregue validaciones y cambie unas cosas

--- Client/src/Views/LoginAdmin/LoginAdmin.jsx
+++ Client/src/Views/LoginAdmin/LoginAdmin.jsx
@@ -2,12 +2,28 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { login } from "../../Redux/actions";
 import { useNavigate } from "react-router-dom";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form } from "formik";
 
 const initialValues = {
   email: "",
   password: "",
   rol: "admin",
+};
+
+const validate = (values) => {
+  const errors = {};
+
+  if (!values.email) {
+    errors.email = "Campo obligatorio";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = "Dirección de correo electrónico no válida";
+  }
+
+  if (!values.password) {
+    errors.password = "Campo obligatorio";
+  }
+
+  return errors;
 };
 
 const LoginAdmin = () => {
@@ -21,16 +37,20 @@ const LoginAdmin = () => {
 
   return (
     <div>
-      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-        <Form>
-          <label htmlFor="email">Email: </label>
-          <Field type="text" name="email" />
+      <Formik initialValues={initialValues} onSubmit={handleSubmit} validate={validate}>
+        {({ errors, touched }) => (
+          <Form>
+            <label htmlFor="email">Email: </label>
+            <input type="text" id="email" name="email" />
+            {errors.email && touched.email && <div>{errors.email}</div>}
 
-          <label htmlFor="password">Password: </label>
-          <Field type="text" name="password" />
+            <label htmlFor="password">Password: </label>
+            <input type="text" id="password" name="password" />
+            {errors.password && touched.password && <div>{errors.password}</div>}
 
-          <button type="submit">Send</button>
-        </Form>
+            <button type="submit">Send</button>
+          </Form>
+        )}
       </Formik>
     </div>
   );
